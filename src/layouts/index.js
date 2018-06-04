f643a067ae1d94e70bdcf655a62ac356ac6a638a
Jakub Roman <jakubro@users.noreply.github.com>
2018-06-04 13:40:28 +0200

    description

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Helmet from 'react-helmet';
 import 'tachyons/css/tachyons.min.css';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -12,6 +13,9 @@ export default class Template extends React.Component {
   render() {
     return (
       <div className='avenir'>
+        <Helmet>
+          <meta name="description" content='Tech enthusiast. Coffee addict. Cat person. Read a lot. Concise.' />
+        </Helmet>
         <Header />
         {this.props.children()}
         <Footer />
