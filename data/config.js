49116ee72c52eec09eed947eecbcbd2ab16d5fa4
Jakub Roman <jakubro@users.noreply.github.com>
2019-02-06 10:40:17 +0100

    change the tagline
    Want to avoid confusion with the term, whose connotation was "slightly" modified in the famous New Yorker's short story "Cat Person."
    
    BTW, it's a refreshing reading and, spoiler alert, it's not about cats. So if you haven't read that yet, then do it now.
    
    https://www.newyorker.com/magazine/2017/12/11/cat-person

--- data/config.js
+++ data/config.js
@@ -3,5 +3,5 @@
 module.exports = {
   siteTitle: 'Jakub Roman',
   siteAuthor: 'Jakub Roman',
-  siteDescription: 'Tech enthusiast. Coffee addict. Cat person. Avid reader.'
+  siteDescription: 'Tech enthusiast. Coffee addict. Cat lover. Avid reader.'
 };
