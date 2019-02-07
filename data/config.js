d0037c4557531cd8d4467e368ab3252d0b95b3f4
Jakub Roman <jakubro@users.noreply.github.com>
2019-02-07 12:49:34 +0100

    site description with non breaking spaces

--- data/config.js
+++ data/config.js
@@ -3,5 +3,7 @@
 module.exports = {
   siteTitle: 'Jakub Roman',
   siteAuthor: 'Jakub Roman',
-  siteDescription: 'Tech enthusiast. Coffee addict. Cat lover. Avid reader.'
+  siteDescription: 'Tech enthusiast. Coffee addict. Cat lover. Avid reader.',
+  // \u00A0 is non-breaking space. Not using &nbsp; because JSX would output it literally.
+  siteDescriptionHtml: 'Tech\u00A0enthusiast. Coffee\u00A0addict. Cat\u00A0lover. Avid\u00A0reader.',
 };
